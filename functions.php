83568fae3e5680e82a36030608b0b3cfa5dc9607
Nico <nicolas.venegas@gmail.com>
2026-08-06 21:11:16 -0400

    add taxonomies

--- functions.php
+++ functions.php
@@ -1,3 +1,4 @@
 <?php
 
 require_once get_stylesheet_directory() . '/inc/acf-project-meta.php';
+require_once get_stylesheet_directory() . '/inc/project-taxonomies.php';
---
+++ inc/project-taxonomies.php
@@ -0,0 +1,80 @@
+<?php
+
+function losojos_project_taxonomies_shortcode() {
+
+    if (!is_singular('proyecto')) {
+        return '';
+    }
+
+    ob_start();
+
+    ?>
+
+    <div class="project-taxonomies">
+
+
+        <?php
+
+        $taxonomies = [
+            'tipo-de-proyecto' => 'Tipo de proyecto',
+            'disciplinas' => 'Disciplinas',
+            'project_topics' => 'Temas'
+        ];
+
+
+        foreach ($taxonomies as $taxonomy => $label) :
+
+            $terms = get_the_terms(
+                get_the_ID(),
+                $taxonomy
+            );
+
+
+            if ($terms && !is_wp_error($terms)) :
+
+        ?>
+
+            <div class="project-taxonomy-item">
+
+                <strong>
+                    <?php echo esc_html($label); ?>:
+                </strong>
+
+
+                <div class="project-taxonomy-terms">
+
+                    <?php foreach ($terms as $term) : ?>
+
+                        <a href="<?php echo esc_url(get_term_link($term)); ?>">
+                            <?php echo esc_html($term->name); ?>
+                        </a>
+
+                    <?php endforeach; ?>
+
+                </div>
+
+            </div>
+
+
+        <?php
+
+            endif;
+
+        endforeach;
+
+        ?>
+
+
+    </div>
+
+    <?php
+
+    return ob_get_clean();
+
+}
+
+
+add_shortcode(
+    'project_taxonomies',
+    'losojos_project_taxonomies_shortcode'
+);
